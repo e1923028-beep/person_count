94b6799e07c2ef0ecdffe5468513fa6da8a508c1
e1923028-beep <e1923028@oit.ac.jp>
2026-04-26 17:28:39 +0900

    Add files via upload

--- app1.py
+++ app1.py
@@ -3,6 +3,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from streamlit_autorefresh import st_autorefresh
 import base64, pathlib
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="人数カウント × ゲーム", layout="wide")
 st_autorefresh(interval=5000, key="data_refresh")
@@ -36,16 +37,25 @@ with col1:
         st.info(f"この人数（{cell_value}球）でゲームが遊べます →")
     except Exception as e:
         st.error(f"データ取得エラー: {e}")
-
 with col2:
-    st.subheader("🎮 人数シューティングゲーム")
-    # GodotをHTML5でエクスポートしたファイルのパスを指定
-    game_path = pathlib.Path("./game/index.html")
-    if game_path.exists():
-        st.components.v1.html(
-            game_path.read_text(encoding="utf-8"),
-            height=600,
-            scrolling=False
-        )
-    else:
-        st.warning("ゲームファイルが見つかりません。`./export/index.html` にGodotのHTML5エクスポートを配置してください。")
+    st.title("プーさんのホームランダービー⚾")
+    
+    # さっきGitHub Pagesで作ったURL + exportフォルダ + index.html
+    # URLの末尾に「?count=人数」をつけてデータを渡す
+    base_url = "https://e1923028-beep.github.io/person_count/game/index.html"
+    game_url = f"{base_url}?count={cell_value}"
+    
+    # URLを指定してiframeで埋め込む
+    components.iframe(game_url, width=800, height=600)
+# with col2:
+#     st.subheader("🎮 人数シューティングゲーム")
+#     # GodotをHTML5でエクスポートしたファイルのパスを指定
+#     game_path = pathlib.Path("./game/index.html")
+#     if game_path.exists():
+#         st.components.v1.html(
+#             game_path.read_text(encoding="utf-8"),
+#             height=600,
+#             scrolling=False
+#         )
+#     else:
+#         st.warning("ゲームファイルが見つかりません。`./export/index.html` にGodotのHTML5エクスポートを配置してください。")
